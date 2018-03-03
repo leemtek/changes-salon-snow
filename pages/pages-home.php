c9db7ef8d7f5e689970d2fdba4a761085b733918
Duane Leem <duaneleem@alumni.ggu.edu>
2018-03-03 17:39:40 +0000

    Added vote banner from Felicia.

--- pages/pages-home.php
+++ pages/pages-home.php
@@ -22,9 +22,14 @@ get_header(); ?>
                         <li data-target="#carousel-example-generic" data-slide-to="4"></li>
                         <li data-target="#carousel-example-generic" data-slide-to="5"></li>
                         <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="7"></li>
                     </ol>
                     <div class="carousel-inner" role="listbox">
                         <div class="item active">
+                            <a href="https://l.messenger.com/l.php?u=https%3A%2F%2Fchangessalon.com%2Fdiablo-magazine-best-east-bay%2F&h=ATO2ddY041bttw5t3F7i3VyAkJR2ytvNIfcNA0pX9HkM05_DkOaRwz2-5z4qdHy0HjyeBcvys55rc9WAwZahJBb1ZGNTGyaZ6BFCPuzhVx01poFzjnoy_Kr4"><img src="https://d1xrp9zhb3ks3c.cloudfront.net/web/changessalon/img/promos/20180303_vote.jpg" alt="Vote Best for Best Salon and Day Spa" /></a>
+                        </div>
+                      
+                        <div class="item">
                             <img src="https://d1xrp9zhb3ks3c.cloudfront.net/web/changessalon/2016/images/walnut-creek-salon-spa.jpg" alt="Walnut Creek Salon Spa">
                         </div>
                         <div class="item">
